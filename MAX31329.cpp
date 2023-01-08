ba320ea980286b5d735a927ce6cf9696e32935b6
jimmyjames <jeggleston1981@users.noreply.github.com>
2023-01-09 06:50:22 +0800

    Add files via upload

---
+++ MAX31329.cpp
@@ -0,0 +1,116 @@
+// Arduino MAX31329 Library
+// Copyright (C) 2023 by James Eggleston
+
+#include <MAX31329.h>
+
+void MAX31329::begin(int SDA, int SCL)
+{
+	Wire.begin(SDA, SCL);
+}
+
+//Returns 0 if Successful transaction on I2C bus
+uint8_t MAX31329::read(tmElements_t &tm)
+{
+	Wire.beginTransmission(0x68);
+  Wire.write(0x06);
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x68, 7); //request 7 bytes (secs, min, hr, dow, date, mth, yr)
+  while(Wire.available()){
+  	tm.Second = bcd2dec(Wire.read());
+  	tm.Minute = bcd2dec(Wire.read());
+  	tm.Hour = bcd2dec(Wire.read() & ~_BV(MAX_HR1224));
+  	tm.Wday = Wire.read();
+  	tm.Day = bcd2dec(Wire.read());
+  	tm.Month = bcd2dec(Wire.read() & ~_BV(MAX_CENTURY));
+  	tm.Year = tmYearToY2k(bcd2dec(Wire.read()));
+	}
+  return 0;
+}
+
+uint8_t MAX31329::write(tmElements_t &tm)
+{
+	Wire.beginTransmission(0x68);
+  Wire.write(0x06);
+	Wire.write(dec2bcd(tm.Second));
+	Wire.write(dec2bcd(tm.Minute));
+	Wire.write(dec2bcd(tm.Hour));
+	Wire.write(tm.Wday);
+	Wire.write(dec2bcd(tm.Day));
+	Wire.write(dec2bcd(tm.Month));
+	Wire.write(dec2bcd(y2kYearToTm(tm.Year)));
+	Wire.endTransmission();
+  return 0;
+}
+
+uint8_t MAX31329::writeRTC(uint8_t addr, uint8_t* values, uint8_t nBytes)
+{
+    Wire.beginTransmission(MAX_ADR);
+    Wire.write(addr);
+    for (uint8_t i=0; i<nBytes; i++) Wire.write(values[i]);
+    return Wire.endTransmission();
+}
+
+uint8_t MAX31329::writeRTC(uint8_t addr, uint8_t value)
+{
+    return ( writeRTC(addr, &value, 1) );
+}
+
+uint8_t MAX31329::readRTC(uint8_t addr, uint8_t* values, uint8_t nBytes)
+{
+    Wire.beginTransmission(MAX_ADR);
+    Wire.write(addr);
+    if ( uint8_t e = Wire.endTransmission(false) ) return e;
+    Wire.requestFrom( (uint8_t)MAX_ADR, nBytes );
+    for (uint8_t i=0; i<nBytes; i++) values[i] = Wire.read();
+    return 0;
+}
+
+uint8_t MAX31329::readRTC(uint8_t addr)
+{
+    uint8_t b {0};
+
+    readRTC(addr, &b, 1);
+    return b;
+}
+
+// Set Output Clock Frequency on INTB/CLKOUT
+void MAX31329::squareWave(SQWAVE_FREQ freq)
+{
+    uint8_t config2 = readRTC(MAX_CONFIG2);
+    if (freq >= SQWAVE_NONE) {
+        config2 & _BV(MAX_CLKO); //set bit 7 to 1 to disable Square Wave and enable INT
+    }
+    else {
+        config2 = (config2 & 0x1F) | ((0x04 | freq ) << CLKO_HZ); // Set output frequency
+    }
+    writeRTC(MAX_CONFIG2, config2);
+}
+
+void MAX31329::enableCharger(bool charge)
+{
+    uint8_t enable = 0b10000001;
+    uint8_t disable = 0b00000001;
+    if(charge){
+        Wire.beginTransmission(MAX_ADR);
+        Wire.write(MAX_CHARGER);
+        Wire.write(enable);
+        Wire.endTransmission();
+    }else {
+        Wire.beginTransmission(MAX_ADR);
+        Wire.write(MAX_CHARGER);
+        Wire.write(disable);
+        Wire.endTransmission();
+    }
+}
+
+// Decimal-to-BCD conversion
+uint8_t MAX31329::dec2bcd(uint8_t n)
+{
+    return n + 6 * (n / 10);
+}
+
+// BCD-to-Decimal conversion
+uint8_t __attribute__ ((noinline)) MAX31329::bcd2dec(uint8_t n)
+{
+    return n - 6 * (n >> 4);
+}
---
+++ MAX31329.h
@@ -0,0 +1,93 @@
+#ifndef DS3232RTC_H_INCLUDED
+#define DS3232RTC_H_INCLUDED
+
+#include <Arduino.h>
+#include <TimeLib.h> 
+#include <Wire.h>
+
+#ifndef _BV
+#define _BV(bit) (1 << (bit))
+#endif
+
+class MAX31329
+{
+	public:
+		enum SQWAVE_FREQ {
+			SQW_1_HZ,
+			SQW_4096_HZ,
+			SQW_8192_HZ,
+			SQW_32768_HZ,
+			SQWAVE_NONE
+		};
+
+		enum CHARGER {
+			K3_SCHOTTKY,
+			K6_SCHOTTKY,
+			K11_SCHOTTKY,
+			K3_SCHOTTKY_DO,
+			K6_SCHOTTKY_DO,
+			K11_SCHOTTKY_DO
+		};
+
+		enum ALARM_TYPES {
+			A1_ONCE_SECOND,
+			A1_SEC_MATCH,
+			A1_MIN_SEC_MATCH,
+			A1_HOUR_MIN_SEC_MATCH,
+			A1_DATE_HOUR_MIN_SEC_MATCH,
+			A1_MONTH_DATE_HOUR_MIN_SEC_MATCH,
+			A1_YEAR_MONTH_DATE_HOUR_MIN_SEC_MATCH,
+			A1_DAY_HOUR_MIN_SEC_MATCH,
+			A2_ONCE_MINUTE,
+			A2_MIN_MATCH,
+			A2_HOUR_MIN_MATCH,
+			A2_DATE_HOUR_MIN_MATCH,
+			A2_DAY_HOUR_MIN_MATCH
+		};
+
+		enum ALARM_NUM {
+			ALARM_1 = 1,
+			ALARM_2 = 2
+		};
+
+		static constexpr uint8_t
+			MAX_ADR				{0x68},
+			MAX_STATUS			{0x00},
+			MAX_INT_EN			{0x01},
+			MAX_RTC_RESET		{0x02},
+			MAX_CONFIG			{0x03},
+			MAX_CONFIG2			{0x04},
+			MAX_TIMER_CONFIG	{0x05},
+			MAX_SECONDS			{0x06},
+			MAX_MINUTES			{0x07},
+			MAX_HOURS			{0x08},
+			MAX_DAY				{0x09},
+			MAX_DATE			{0x0A},
+			MAX_MONTH			{0x0B},
+			MAX_YEAR			{0x0C},
+			MAX_PWR_MANAGE		{0x18},
+			MAX_CHARGER			{0x19},
+			CLKO_HZ				{5},
+			MAX_CLKO			{7},
+			MAX_HR1224		    {6},        // Hours register 12 or 24 hour mode (24 hour mode==0)
+        	MAX_CENTURY     	{7};
+
+    	MAX31329() {};
+    	void begin(int SDA, int SCL);
+    	//static time_t get();
+    	//uint8_t set(time_t t);
+    	static uint8_t read(tmElements_t &tm);
+    	uint8_t write(tmElements_t &tm);
+    	void squareWave(SQWAVE_FREQ freq);
+    	void enableCharger(bool charge);
+    	uint8_t readRTC(uint8_t addr, uint8_t* values, uint8_t nBytes);
+    	uint8_t readRTC(uint8_t addr);
+    	uint8_t writeRTC(uint8_t addr, uint8_t* values, uint8_t nBytes);
+    	uint8_t writeRTC(uint8_t addr, uint8_t value);
+
+	private:
+    	uint8_t dec2bcd(uint8_t n);
+    	static uint8_t bcd2dec(uint8_t n);
+};
+
+#endif
